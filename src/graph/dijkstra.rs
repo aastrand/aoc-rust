19fff8666ae5e4b1dc394c05c0336300d5cf6355
Anders Åstrand <anders@teknodromen.se>
2022-08-15 19:38:23 +0200

    Add dijkstra

---
+++ src/graph/dijkstra.rs
@@ -0,0 +1,109 @@
+use std::collections::{HashMap, HashSet};
+use std::hash::Hash;
+
+pub fn dijkstra<T>(
+    start: &'static T,
+    end: &'static T,
+    graph: &HashMap<&'static T, Vec<&'static T>>,
+) -> (
+    Vec<&'static T>,
+    HashMap<&'static T, f64>,
+    HashMap<&'static T, &'static T>,
+)
+where
+    T: Eq + Hash,
+{
+    let mut q: HashSet<&'static T> = HashSet::new();
+    let mut dist: HashMap<&'static T, f64> = HashMap::new();
+    let mut prev: HashMap<&'static T, &'static T> = HashMap::new();
+    let empty: Vec<&'static T> = vec![];
+
+    for pos in graph.keys() {
+        dist.insert(pos, f64::INFINITY);
+        q.insert(pos);
+    }
+    dist.insert(start, 0.0);
+
+    while q.len() > 0 {
+        if let Some(u) = get_min_dist(&q, &dist) {
+            q.remove(u);
+
+            for n in graph.get(u).unwrap_or(&empty) {
+                let alt = dist.get(u).unwrap_or(&f64::INFINITY) + 1.0;
+                if alt < *dist.get(n).unwrap_or(&f64::INFINITY) {
+                    dist.insert(n, alt);
+                    prev.insert(n, u);
+                }
+            }
+        }
+    }
+
+    let mut path = vec![];
+    let mut u = end;
+    if prev.contains_key(u) || u == start {
+        loop {
+            path.insert(0, u);
+            if let Some(next) = prev.get(u) {
+                u = next;
+            } else {
+                break;
+            }
+        }
+    }
+
+    (path, dist, prev)
+}
+
+fn get_min_dist<T>(q: &HashSet<&'static T>, dist: &HashMap<&'static T, f64>) -> Option<&'static T>
+where
+    T: Eq + Hash,
+{
+    let mut min = f64::INFINITY;
+    let mut min_node = None;
+    for v in q {
+        if let Some(d) = dist.get(*v) {
+            if *d < min {
+                min = *d;
+                min_node = Some(*v);
+            }
+        }
+    }
+
+    min_node
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_dijkstra_empty() {
+        let graph = HashMap::new();
+
+        let (path, dist, prev) = dijkstra(&0, &10, &graph);
+        assert_eq!(0, path.len());
+        assert_eq!(1, dist.len());
+        assert_eq!(0, prev.len());
+    }
+
+    #[test]
+    fn test_dijkstra() {
+        let mut graph = HashMap::new();
+        graph.insert(&0, vec![&1, &5]);
+        graph.insert(&1, vec![&0, &2]);
+        graph.insert(&2, vec![&1, &3, &4]);
+        graph.insert(&3, vec![&2, &4]);
+        graph.insert(&4, vec![&2, &10]);
+        graph.insert(&5, vec![&0, &6, &7]);
+        graph.insert(&6, vec![&5, &7, &8, &10]);
+        graph.insert(&7, vec![&5, &6, &8]);
+        graph.insert(&8, vec![&6, &7, &9]);
+        graph.insert(&9, vec![&8]);
+        graph.insert(&10, vec![&4, &6]);
+
+        let (path, dist, prev) = dijkstra(&0, &10, &graph);
+        assert_eq!(vec![&0, &5, &6, &10], path);
+        assert_eq!(Some(&&6), prev.get(&10));
+        assert_eq!(Some(&2.0), dist.get(&6));
+    }
+}
--- src/graph/mod.rs
+++ src/graph/mod.rs
@@ -1,28 +1,33 @@
+pub mod dijkstra;
+
 use std::collections::{HashMap, HashSet, VecDeque};
-use std::fmt::Display;
 use std::hash::Hash;
 
-pub fn bfs<T, F>(start: &T, graph: &HashMap<T, Vec<T>>, mut visitor: F) -> HashSet<T>
+pub fn bfs<T, F>(
+    start: &'static T,
+    graph: &HashMap<&'static T, Vec<&'static T>>,
+    mut visitor: F,
+) -> HashSet<&'static T>
 where
-    T: Clone + Copy + Eq + Hash + Display,
-    F: FnMut(T),
+    T: Eq + Hash,
+    F: FnMut(&'static T),
 {
-    let mut visited = HashSet::new();
-    let mut q: VecDeque<T> = VecDeque::new();
+    let mut visited: HashSet<&'static T> = HashSet::new();
+    let mut q: VecDeque<&'static T> = VecDeque::new();
 
-    q.push_back(*start);
-    visited.insert(*start);
-    visitor(*start);
+    q.push_back(start);
+    visited.insert(start);
+    visitor(start);
     let empty = vec![];
 
     while q.len() > 0 {
         let cur = q.pop_front().unwrap();
-        let neighbours = graph.get(&cur).unwrap_or(&empty);
+        let neighbours = graph.get(cur).unwrap_or(&empty);
         for n in neighbours {
             if !visited.contains(n) {
-                q.push_back(*n);
-                visited.insert(*n);
-                visitor(*n);
+                q.push_back(n);
+                visited.insert(n);
+                visitor(n);
             }
         }
     }
@@ -30,30 +35,34 @@ where
     visited
 }
 
-pub fn dfs<T, F>(start: &T, graph: &HashMap<T, Vec<T>>, mut visitor: F) -> HashSet<T>
+pub fn dfs<T, F>(
+    start: &'static T,
+    graph: &HashMap<&'static T, Vec<&'static T>>,
+    mut visitor: F,
+) -> HashSet<&'static T>
 where
-    T: Clone + Copy + Eq + Hash + Display,
-    F: FnMut(T),
+    T: Eq + Hash + Sized,
+    F: FnMut(&'static T),
 {
-    let mut visited = HashSet::new();
-    let mut q: VecDeque<T> = VecDeque::new();
+    let mut visited: HashSet<&'static T> = HashSet::new();
+    let mut q: VecDeque<&'static T> = VecDeque::new();
 
-    q.push_back(*start);
-    visited.insert(*start);
-    visitor(*start);
+    q.push_back(start);
+    visited.insert(start);
+    visitor(start);
     let empty = vec![];
 
     while q.len() > 0 {
         let cur = q.pop_back().unwrap();
-        if !visited.contains(&cur) {
+        if !visited.contains(cur) {
             visited.insert(cur);
             visitor(cur);
         }
 
-        let neighbours = graph.get(&cur).unwrap_or(&empty);
+        let neighbours = graph.get(cur).unwrap_or(&empty);
         for n in neighbours {
             if !visited.contains(n) {
-                q.push_back(*n);
+                q.push_back(n);
             }
         }
     }
@@ -73,30 +82,30 @@ mod tests {
 
     #[test]
     fn test_bfs_traverse() {
-        let mut graph = HashMap::new();
-        graph.insert(0, vec![1, 2]);
-        graph.insert(1, vec![0, 2, 3, 4]);
-        graph.insert(2, vec![0, 1, 3, 5]);
-        graph.insert(3, vec![1, 2, 4, 5]);
-        graph.insert(4, vec![2, 3]);
-        graph.insert(5, vec![1, 3]);
-        graph.insert(6, vec![7, 8]);
-        graph.insert(7, vec![6]);
-        graph.insert(8, vec![6]);
+        let mut graph: HashMap<&i64, Vec<&i64>> = HashMap::new();
+        graph.insert(&0, vec![&1, &2]);
+        graph.insert(&1, vec![&0, &2, &3, &4]);
+        graph.insert(&2, vec![&0, &1, &3, &5]);
+        graph.insert(&3, vec![&1, &2, &4, &5]);
+        graph.insert(&4, vec![&2, &3]);
+        graph.insert(&5, vec![&1, &3]);
+        graph.insert(&6, vec![&7, &8]);
+        graph.insert(&7, vec![&6]);
+        graph.insert(&8, vec![&6]);
 
         let mut order = vec![];
         let visited = bfs(&0, &graph, |n| order.push(n));
         assert_eq!(6, visited.len());
         assert!(visited.contains(&5));
         assert!(!visited.contains(&6));
-        assert_eq!(vec![0, 1, 2, 3, 4, 5], order);
+        assert_eq!(vec![&0, &1, &2, &3, &4, &5], order);
 
         order = vec![];
         let visited = bfs(&6, &graph, |n| order.push(n));
         assert_eq!(3, visited.len());
         assert!(visited.contains(&6));
         assert!(!visited.contains(&5));
-        assert_eq!(vec![6, 7, 8], order);
+        assert_eq!(vec![&6, &7, &8], order);
     }
 
     #[test]
@@ -107,29 +116,29 @@ mod tests {
 
     #[test]
     fn test_dfs_traverse() {
-        let mut graph = HashMap::new();
-        graph.insert(0, vec![1, 2]);
-        graph.insert(1, vec![0, 2, 3, 4]);
-        graph.insert(2, vec![0, 1, 3, 5]);
-        graph.insert(3, vec![1, 2, 4, 5]);
-        graph.insert(4, vec![2, 3]);
-        graph.insert(5, vec![1, 3]);
-        graph.insert(6, vec![7, 8]);
-        graph.insert(7, vec![6]);
-        graph.insert(8, vec![6]);
+        let mut graph: HashMap<&'static &str, Vec<&'static &str>> = HashMap::new();
+        graph.insert(&"0", vec![&"1", &"2"]);
+        graph.insert(&"1", vec![&"0", &"2", &"3", &"4"]);
+        graph.insert(&"2", vec![&"0", &"1", &"3", &"5"]);
+        graph.insert(&"3", vec![&"1", &"2", &"4", &"5"]);
+        graph.insert(&"4", vec![&"2", &"3"]);
+        graph.insert(&"5", vec![&"1", &"3"]);
+        graph.insert(&"6", vec![&"7", &"8"]);
+        graph.insert(&"7", vec![&"6"]);
+        graph.insert(&"8", vec![&"6"]);
 
         let mut order = vec![];
-        let visited = dfs(&0, &graph, |n| order.push(n));
+        let visited = dfs(&"0", &graph, |n| order.push(n));
         assert_eq!(6, visited.len());
-        assert!(visited.contains(&5));
-        assert!(!visited.contains(&6));
-        assert_eq!(vec![0, 2, 5, 3, 4, 1], order);
+        assert!(visited.contains(&"5"));
+        assert!(!visited.contains(&"6"));
+        assert_eq!(vec![&"0", &"2", &"5", &"3", &"4", &"1"], order);
 
         order = vec![];
-        let visited = dfs(&6, &graph, |n| order.push(n));
+        let visited = dfs(&"6", &graph, |n| order.push(n));
         assert_eq!(3, visited.len());
-        assert!(visited.contains(&6));
-        assert!(!visited.contains(&5));
-        assert_eq!(vec![6, 8, 7], order);
+        assert!(visited.contains(&"6"));
+        assert!(!visited.contains(&"5"));
+        assert_eq!(vec![&"6", &"8", &"7"], order);
     }
 }
